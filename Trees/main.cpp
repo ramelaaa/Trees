9514e6388b22ddb30dfdd9537a3a1adf22e8d5a6
Ramela Ramnauth <ramelaramnauth2@gmail.com>
2020-08-25 09:37:40 -0400

    Initial Commit

---
+++ Trees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trees
+//
+//  Created by Ramela Ramnauth on 8/25/20.
+//  Copyright © 2020 Ramela Ramnauth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
